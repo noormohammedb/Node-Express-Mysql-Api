6606cc90a9d122dd3507795201a194b2ed929f26
noormohammedb <nooru.mohad@gmail.com>
2020-09-13 00:42:25 +0530

    database connection sucess

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+const dbCredentials = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'expresslogin'
+};
+const dbConnection = mysql.createConnection(dbCredentials);
+dbConnection.connect((error)=>{
+  if(error) throw error;
+    console.log('db connection sucess');
+});
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+/* GET data */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
 });
 
 module.exports = router;
